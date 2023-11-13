30eabe277c390f6931c57ca7e5c068637f6e1972
snowlabo001 <snowlabo001@gmail.com>
2023-11-13 14:19:57 +0900

    prop-types

--- src/Hello.js
+++ src/Hello.js
@@ -1,18 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Ddubi({who}){
+function Ddubi({name, job}){
   return(
-      <h1>Do you know {who}?</h1>    
+      <div><h1>? Do you know {name}?</h1><h2>I'm a(an) {job}.</h2></div>    
   )
 }
+
+const doYouKnow = [
+  {
+    id : 1,
+    name : "me",
+    job : "programmer" 
+  }, 
+  {
+    id : 2,
+    name : "SnowMan",
+    job : "idol" 
+  }, 
+  {
+    id : 3,
+    name : "luffy",
+    job : "pirate" 
+  }
+]
+
+Ddubi.propTypes = {
+  name : PropTypes.string.isRequired,
+  job : PropTypes.string.isRequired
+}
+
+/* function person(who){
+  return <Ddubi name={who.name} job={who.job}/>;
+}*/
 
 function Hello() {
   return (
     <div>
       <h1>hello, world!!!</h1>
-      <Ddubi who='DDUBI'/>
+      {/* {doYouKnow.map(person)} */}
+      {doYouKnow.map(who=><Ddubi key={who.id} name={who.name} job={who.job}/>)}
+      {/* <Ddubi who='me'/>
       <Ddubi who='SnowMan'/>
-      <Ddubi who='React'/>
+      <Ddubi who='React'/> */}
     </div>
   );
 }
